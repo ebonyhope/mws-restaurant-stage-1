25e94dae22712a8752c8eb0477b76949ae32e40a
ebonyhope <temylope@gmail.com>
2018-08-19 01:35:16 +0100

    added my maps

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZWJvbnlob3BlIiwiYSI6ImNqa3hvcTdkMTBhMG0zdm53bXc2cmU0bnIifQ.bgsJvtYPmqSk2SoiF62Ulw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
